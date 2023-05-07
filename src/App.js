18008d6df1f2bba09da2ca0293bf3671b9c7883a
Leonardo Alessandrini <aless.leon7@gmail.com>
2023-05-07 17:09:48 +0200

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import './App.css';
-import logo from './logo.svg';
+import { css } from '@emotion/react';
+import col from 'randomcolor';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
+const button = css`
+  background-color: yellow;
+  border-radius: 8px;
+  padding: 6px;
+  width: 100px;
+`;
 
 export default function App() {
+  const randColor = col.randomColor({});
+
+  const [color, setColor] = useState(randomColor);
+  const [width, setWidth] = useState('');
+  const [height, setHeight] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'grid',
+        placeItems: 'center',
+      }}
+    >
+      <h1>Random Color Generator</h1>
+      <div
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+          display: 'grid',
+          placeItems: 'center',
+          marginBottom: '10px',
+          background: color,
+          // // Smooth Color Transition:
+          transition: 'background 0.5s ease-in-out',
+          padding: '3px',
+        }}
+      >
+        Generated Color: {color}
+      </div>
+      <button
+        onClick={() => {
+          setColor(randColor);
+          console.log('Generated Color:', randColor);
+        }}
+        css={button}
+      >
+        Generate
+      </button>
     </div>
   );
 }
